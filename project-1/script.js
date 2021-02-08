13e328471adc7fe8561548c2e4fd2109c49c9569
asharao99 <77996456+asharao99@users.noreply.github.com>
2021-02-08 06:59:36 -0600

    project 1 final commit

---
+++ project-1/script.js
@@ -0,0 +1,64 @@
+$(document).ready(function () {
+$(".alphasc").click(function() {
+     $("#alpha").css('display', 'grid');  
+  });
+$("#alpha").click(function() {
+     $("#alpha").css('display', 'none');  
+  });
+    
+$(".annasc").click(function() {
+     $("#anna").css('display', 'grid');  
+  });
+$("#anna").click(function() {
+     $("#anna").css('display', 'none');  
+  });
+    
+$(".annesc").click(function() {
+     $("#anne").css('display', 'grid');  
+  });
+$("#anne").click(function() {
+     $("#anne").css('display', 'none');  
+  });
+    
+$(".anne2sc").click(function() {
+     $("#anne2").css('display', 'grid');  
+  });
+$("#anne2").click(function() {
+     $("#anne2").css('display', 'none');  
+  });
+    
+$(".anne3sc").click(function() {
+     $("#anne3").css('display', 'grid');  
+  });
+$("#anne3").click(function() {
+     $("#anne3").css('display', 'none');  
+  });
+    
+$(".fallonsc").click(function() {
+     $("#fallon").css('display', 'grid');  
+  });
+$("#fallon").click(function() {
+     $("#fallon").css('display', 'none');  
+  });
+    
+$(".nammusc").click(function() {
+     $("#nammu").css('display', 'grid');  
+  });
+$("#nammu").click(function() {
+     $("#nammu").css('display', 'none');  
+  });
+    
+$(".hornetsc").click(function() {
+     $("#hornet").css('display', 'grid');  
+  });
+$("#hornet").click(function() {
+     $("#hornet").css('display', 'none');  
+  });
+    
+$(".prepssc").click(function() {
+     $("#preps").css('display', 'grid');  
+  });
+$("#preps").click(function() {
+     $("#preps").css('display', 'none');  
+  });
+});
