24d158c84e2145b811621627200c6abacb7f4264
asharao99 <77996456+asharao99@users.noreply.github.com>
2021-03-01 11:31:07 -0600

    project-1 final commit

--- project-1/script.js
+++ project-1/script.js
@@ -1,64 +1,100 @@
 $(document).ready(function () {
-$(".alphasc").click(function() {
-     $("#alpha").css('display', 'grid');  
-  });
-$("#alpha").click(function() {
-     $("#alpha").css('display', 'none');  
-  });
-    
-$(".annasc").click(function() {
-     $("#anna").css('display', 'grid');  
-  });
-$("#anna").click(function() {
-     $("#anna").css('display', 'none');  
-  });
-    
-$(".annesc").click(function() {
-     $("#anne").css('display', 'grid');  
-  });
-$("#anne").click(function() {
-     $("#anne").css('display', 'none');  
-  });
-    
-$(".anne2sc").click(function() {
-     $("#anne2").css('display', 'grid');  
-  });
-$("#anne2").click(function() {
-     $("#anne2").css('display', 'none');  
-  });
-    
-$(".anne3sc").click(function() {
-     $("#anne3").css('display', 'grid');  
-  });
-$("#anne3").click(function() {
-     $("#anne3").css('display', 'none');  
-  });
-    
-$(".fallonsc").click(function() {
-     $("#fallon").css('display', 'grid');  
-  });
-$("#fallon").click(function() {
-     $("#fallon").css('display', 'none');  
-  });
-    
-$(".nammusc").click(function() {
-     $("#nammu").css('display', 'grid');  
-  });
-$("#nammu").click(function() {
-     $("#nammu").css('display', 'none');  
-  });
-    
-$(".hornetsc").click(function() {
-     $("#hornet").css('display', 'grid');  
-  });
-$("#hornet").click(function() {
-     $("#hornet").css('display', 'none');  
-  });
-    
-$(".prepssc").click(function() {
-     $("#preps").css('display', 'grid');  
-  });
-$("#preps").click(function() {
-     $("#preps").css('display', 'none');  
-  });
+    $(".alphasc").click(function () {
+        $("#alpha").css('display', 'grid');
+    });
+    $("#alpha").click(function () {
+        $("#alpha_info").css('display', 'grid');
+    });
+    $("#alpha_info").click(function () {
+        $("#alpha").css('display', 'none');
+        $("#alpha_info").css('display', 'none');
+    });
+
+    $(".annasc").click(function () {
+        $("#anna").css('display', 'grid');
+    });
+    $("#anna").click(function () {
+        $("#anna_info").css('display', 'grid');
+    });
+    $("#anna_info").click(function () {
+        $("#anna").css('display', 'none');
+        $("#anna_info").css('display', 'none');
+    });
+
+    $(".annesc").click(function () {
+        $("#anne").css('display', 'grid');
+    });
+    $("#anne").click(function () {
+        $("#anne_info").css('display', 'grid');
+    });
+    $("#anne_info").click(function () {
+        $("#anne").css('display', 'none');
+        $("#anne_info").css('display', 'none');
+    });
+
+    $(".anne2sc").click(function () {
+        $("#anne2").css('display', 'grid');
+    });
+    $("#anne2").click(function () {
+        $("#anne2_info").css('display', 'grid');
+    });
+    $("#anne2_info").click(function () {
+        $("#anne2").css('display', 'none');
+        $("#anne2_info").css('display', 'none');
+    });
+
+    $(".anne3sc").click(function () {
+        $("#anne3").css('display', 'grid');
+    });
+    $("#anne3").click(function () {
+        $("#anne3_info").css('display', 'grid');
+    });
+    $("#anne3_info").click(function () {
+        $("#anne3").css('display', 'none');
+        $("#anne3_info").css('display', 'none');
+    });
+
+    $(".fallonsc").click(function () {
+        $("#fallon").css('display', 'grid');
+    });
+    $("#fallon").click(function () {
+        $("#fallon_info").css('display', 'grid');
+    });
+    $("#fallon_info").click(function () {
+        $("#fallon").css('display', 'none');
+        $("#fallon_info").css('display', 'none');
+    });
+
+    $(".nammusc").click(function () {
+        $("#nammu").css('display', 'grid');
+    });
+    $("#nammu").click(function () {
+        $("#nammu_info").css('display', 'grid');
+    });
+    $("#nammu_info").click(function () {
+        $("#nammu").css('display', 'none');
+        $("#nammu_info").css('display', 'none');
+    });
+
+    $(".hornetsc").click(function () {
+        $("#hornet").css('display', 'grid');
+    });
+    $("#hornet").click(function () {
+        $("#hornet_info").css('display', 'grid');
+    });
+    $("#hornet_info").click(function () {
+        $("#hornet").css('display', 'none');
+        $("#hornet_info").css('display', 'none');
+    });
+
+    $(".prepssc").click(function () {
+        $("#preps").css('display', 'grid');
+    });
+    $("#preps").click(function () {
+        $("#preps_info").css('display', 'grid');
+    });
+    $("#preps_info").click(function () {
+        $("#preps").css('display', 'none');
+        $("#preps_info").css('display', 'none');
+    });
 });
